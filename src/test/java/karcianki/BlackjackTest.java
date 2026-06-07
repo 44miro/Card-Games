4d576d2294779b4ec6427225a36fadb383b923cb
janfr <255585@edu.p.lodz.pl>
2026-06-07 22:07:48 +0200

    zrefaktoryzowanie testow do blackjacka

--- src/test/java/karcianki/BlackjackTest.java
+++ src/test/java/karcianki/BlackjackTest.java
@@ -1,59 +1,56 @@
 package karcianki;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BlackjackTest {
-    @Test
-    public void aceShouldBeCountAsElevenIfScoreLessThen11(){
-        Blackjack bj = new Blackjack();
-        Player player = bj.getPlayer();
-        Hand hand = player.getHand();
-        ArrayList<Card> cards = new ArrayList<Card>();
-        cards.add(new Card(Suit.HEARTS, Rank.ACE));
-        cards.add(new Card(Suit.HEARTS, Rank.TEN));
-        hand.setCards(cards);
-        assertEquals(21,bj.countScore());
+
+    private Blackjack bj;
+    private Hand hand;
+
+    @BeforeEach
+    void setUp() {
+        bj = new Blackjack();
+        hand = bj.getPlayer().getHand();
     }
+
+    private void setHand(Card... cards) {
+        ArrayList<Card> cardList = new ArrayList<>();
+        for (Card card : cards) cardList.add(card);
+        hand.setCards(cardList);
+    }
+
+    private Card card(Rank rank) {
+        return new Card(Suit.HEARTS, rank);
+    }
+
+    // --- testy ---
+
     @Test
-    public void aceShouldBeCountAsOneIfScoreMoreThen11(){
-        Blackjack bj = new Blackjack();
-        Player player = bj.getPlayer();
-        Hand hand = player.getHand();
-        ArrayList<Card> cards = new ArrayList<Card>();
-        cards.add(new Card(Suit.HEARTS, Rank.ACE));
-        cards.add(new Card(Suit.HEARTS, Rank.ACE));
-        hand.setCards(cards);
-        assertNotEquals(22,bj.countScore());
+    void ace_plus_ten_equals_21() {
+        setHand(card(Rank.ACE), card(Rank.TEN));
+        assertEquals(21, bj.countScore());
     }
 
     @Test
-    public void aceShouldBeCountAsOneIfScoreMoreThen11EvenIfFirstCardIsAce(){
-        Blackjack bj = new Blackjack();
-        Player player = bj.getPlayer();
-        Hand hand = player.getHand();
-        ArrayList<Card> cards = new ArrayList<Card>();
-        cards.add(new Card(Suit.HEARTS, Rank.ACE));
-        cards.add(new Card(Suit.HEARTS, Rank.TEN));
-        cards.add(new Card(Suit.HEARTS, Rank.SEVEN));
-        hand.setCards(cards);
-        assertEquals(18,bj.countScore());
+    void two_aces_should_not_equal_22() {
+        setHand(card(Rank.ACE), card(Rank.ACE));
+        assertEquals(12, bj.countScore());
     }
 
     @Test
-    public void aceShouldBeCountAsElevenIfScoreLessThen11EvenIfFirstCardIsNotAce(){
-        Blackjack bj = new Blackjack();
-        Player player = bj.getPlayer();
-        Hand hand = player.getHand();
-        ArrayList<Card> cards = new ArrayList<Card>();
-        cards.add(new Card(Suit.HEARTS, Rank.TWO));
-        cards.add(new Card(Suit.HEARTS, Rank.SEVEN));
-        cards.add(new Card(Suit.HEARTS, Rank.ACE));
-        hand.setCards(cards);
-        assertEquals(20,bj.countScore());
+    void ace_ten_seven_equals_18() {
+        setHand(card(Rank.ACE), card(Rank.TEN), card(Rank.SEVEN));
+        assertEquals(18, bj.countScore());
     }
 
+    @Test
+    void two_seven_ace_equals_20() {
+        setHand(card(Rank.TWO), card(Rank.SEVEN), card(Rank.ACE));
+        assertEquals(20, bj.countScore());
+    }
 }
