269f760830a9948826c5bcbc2abd852706710c27
janfr <255585@edu.p.lodz.pl>
2026-06-07 21:09:07 +0200

    TDD-Hand Test 1 accepted

--- src/main/java/karcianki/Hand.java
+++ src/main/java/karcianki/Hand.java
@@ -1,4 +1,15 @@
 package karcianki;
 
+import java.util.List;
+
 public class Hand {
+    private List<Card> Hand;
+
+    public Hand(){
+        Hand  = null;
+    }
+
+    public List<Card> getHand(){
+        return Hand;
+    }
 }
--- src/test/java/karcianki/HandTest.java
+++ src/test/java/karcianki/HandTest.java
@@ -1,6 +1,16 @@
 package karcianki;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HandTest {
+    @Test
+    public void handShouldReturnNullIfEmpty()
+    {
+        Hand hand = new Hand();
+        assertNull(hand.getHand());
+    }
+
 
 
 }
